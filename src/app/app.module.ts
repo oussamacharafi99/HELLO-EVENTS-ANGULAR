ad239630b1ce1f7a04595aa32a3e3319951ab66d
oussama charafi <ou.charafi@gmail.com>
2024-07-29 14:54:11 +0100

    I updated the module app and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroHomeComponent } from './Home/hero-home/hero-home.component';
+import { HeaderHomeComponent } from './Home/header-home/header-home.component';
+import { FoterHomeComponent } from './Home/foter-home/foter-home.component';
+import { SingupComponent } from './Home/singup/singup.component';
+import { LoginComponent } from './Home/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroHomeComponent,
+    HeaderHomeComponent,
+    FoterHomeComponent,
+    SingupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
