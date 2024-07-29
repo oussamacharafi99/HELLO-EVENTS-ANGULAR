3eb127ff6d7861605e6de1e7e1aef69cad0cab68
oussama charafi <ou.charafi@gmail.com>
2024-07-29 14:53:38 +0100

    I tested the create store

---
+++ src/app/Store/Store.ts
@@ -0,0 +1,19 @@
+// import { Action } from "@ngrx/store";
+
+// let initialState = {
+//     n: 0
+// }
+
+// export interface StoreInterface{
+
+// }
+
+// function reducer(state = initialState , action : Action ){
+//     switch(action.type){
+//         case "add" : 
+//         return {
+            
+//         }
+//     }
+
+// }
